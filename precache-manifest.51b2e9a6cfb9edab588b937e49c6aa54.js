bfbf4980f3d7886064c5479c3ca4ffbeb7e1e9fb
arthurggssenac <arthurggs.senac@gmail.com>
2019-12-05 10:11:26 -0300

    Updates

---
+++ precache-manifest.51b2e9a6cfb9edab588b937e49c6aa54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86cc79a70b7eafbca776484fddec933b",
+    "url": "/cadastro-de-estandistas-2/index.html"
+  },
+  {
+    "revision": "7b76f92b63e48f8487a4",
+    "url": "/cadastro-de-estandistas-2/static/css/main.e3bc7eed.chunk.css"
+  },
+  {
+    "revision": "333327791da2141c5e37",
+    "url": "/cadastro-de-estandistas-2/static/js/2.763701bd.chunk.js"
+  },
+  {
+    "revision": "7b76f92b63e48f8487a4",
+    "url": "/cadastro-de-estandistas-2/static/js/main.4c2dcb8b.chunk.js"
+  },
+  {
+    "revision": "5a634eede6825282e74b",
+    "url": "/cadastro-de-estandistas-2/static/js/runtime-main.90a29544.js"
+  }
+]);
